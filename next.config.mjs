00e0a0181a8ec49f02f60445dcefeb15224a1481
manuelperche <manuelperche@gmail.com>
2025-05-02 00:34:23 -0400

    fix: removed config and build erros

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,7 @@ import { withContentCollections } from "@content-collections/next";
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-    unoptimized: false,
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    domains: ["pbs.twimg.com"],
   },
   async headers() {
     return [
